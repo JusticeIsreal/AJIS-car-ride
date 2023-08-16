4218efb560511ac1e1a608e7eb661d5f9c0ff09c
Justice Isreal <justiceyba@gmail.com>
2023-08-16 13:49:59 +0100

    LOGIC: Map integration from mapbox

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,32 @@
+"use client";
 import Booking from "@/components/Booking/Booking";
+import MapBoxMap from "@/components/Map/MapBoxMap";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [currentLocation, setCurrentLocation] = useState<any>();
+  console.log(currentLocation);
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+  const getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition((place) => {
+      setCurrentLocation({
+        lat: place.coords.latitude,
+        lng: place.coords.longitude,
+      });
+    });
+  };
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-3">
         <div>
           <Booking />
         </div>
-        <div className="col-span-2">Map</div>
+        <div className="col-span-2">
+          <MapBoxMap />
+        </div>
       </div>
     </div>
   );
---
+++ app/sign-in/jj.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function jj() {
+  return <div>dv</div>;
+}
+
+export default jj;
